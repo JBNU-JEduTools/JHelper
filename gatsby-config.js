a075f362ea9d2c3bf75a75b6b123b3eab3e9daa9
KIM GYUHO <kyuhokim12@gmail.com>
2024-01-19 15:28:57 +0900

    Update gatsby-config.js
    Signed-off-by: KIM GYUHO <kyuhokim12@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     title: `Document Of JEdutools`,
     name: `JHelper`,
-    siteUrl: `https://jbnu-jedutools.github.io`,
+    siteUrl: `https://jbnu-jedutools.github.io/JHelper`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     social: [
       {
