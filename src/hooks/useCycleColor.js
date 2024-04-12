95f809d73bb33d2e0e98536f99ed588ae93fe79c
KIM GYUHO <kyuhokim12@gmail.com>
2024-04-12 15:11:50 +0900

    색상 모드 전환 Bug Fixed
    Signed-off-by: KIM GYUHO <kyuhokim12@gmail.com>

--- src/hooks/useCycleColor.js
+++ src/hooks/useCycleColor.js
@@ -2,17 +2,16 @@ import { useColorMode } from 'theme-ui';
 import colors from '../gatsby-plugin-theme-ui/colors';
 
 const customColors = Object.keys(colors.modes);
-const modes = ['light', 'dark', ...customColors];
+const modes = ['light', ...customColors];
 
 function useCycleColor() {
   const [colorMode, setColorMode] = useColorMode();
 
   const cycleColorMode = () => {
-    const i = modes.indexOf(colorMode);
-    const n = (i + 1) % modes.length;
+    const i = modes.indexOf(colorMode) + 1;
+    const n = (i) % modes.length;
     setColorMode(modes[n]);
   };
-
   return { cycleColorMode };
 }
 
