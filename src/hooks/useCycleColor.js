7e9eca9af8e8f4a42f92e587b272363090e92d34
KIM GYUHO <kyuhokim12@gmail.com>
2024-04-12 14:58:58 +0900

    Update useCycleColor.js
    Signed-off-by: KIM GYUHO <kyuhokim12@gmail.com>

--- src/hooks/useCycleColor.js
+++ src/hooks/useCycleColor.js
@@ -2,14 +2,14 @@ import { useColorMode } from 'theme-ui';
 import colors from '../gatsby-plugin-theme-ui/colors';
 
 const customColors = Object.keys(colors.modes);
-const modes = ['dark', ...customColors];
+const modes = ['light', 'dark', ...customColors];
 
 function useCycleColor() {
   const [colorMode, setColorMode] = useColorMode();
 
   const cycleColorMode = () => {
     const i = modes.indexOf(colorMode);
-    const n = (i + 1) % modes.length;
+    const n = (i + 2) % modes.length;
     setColorMode(modes[n]);
   };
 
